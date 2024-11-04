9a23e5de4cd9198ab08c2a7cf7ae0bcee08333e1
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-11-04 20:40:17 +0700

    add mouse layer

--- keyboards/geigeigeist/totem/keymaps/fahrezahafiz/keymap.c
+++ keyboards/geigeigeist/totem/keymaps/fahrezahafiz/keymap.c
@@ -117,6 +117,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_NO,    KC_NO,    KC_NO,     KC_ENT,   KC_BSPC,  KC_DEL
     ),
 
+    [_MOUSE] = LAYOUT(
+ //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
+              KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_WH_U,  KC_MS_U,  KC_WH_D,  KC_NO,  
+              KC_LGUI,  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_NO,     KC_NO,    KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_NO,    
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,     KC_NO,    KC_WH_L,  KC_NO,    KC_WH_R,  KC_NO,    KC_NO,
+                                  KC_NO,    KC_NO,    KC_NO,     KC_BTN1,  KC_BTN2,  KC_BTN3
+    )
+
 /*
    ╺━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╸
 
