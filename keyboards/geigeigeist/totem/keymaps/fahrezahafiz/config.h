f4eec90117509329dc305e3cd2368f38625eed0a
Fahreza Hafiz <fahreza_12@apps.ipb.ac.id>
2024-11-13 08:17:17 +0700

    enable mouse key

--- keyboards/geigeigeist/totem/keymaps/fahrezahafiz/config.h
+++ keyboards/geigeigeist/totem/keymaps/fahrezahafiz/config.h
@@ -10,13 +10,7 @@ for more options.
 
 #define TAPPING_TERM 170
 
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-// #define TAPPING_FORCE_HOLD
-
-// Auto Shift
-// #define NO_AUTO_SHIFT_ALPHA
-// #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-// #define AUTO_SHIFT_NO_SETUP
+#define MOUSE_EXTENDED_REPORT
 
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
